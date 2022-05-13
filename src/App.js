aa66076b45f2e1af723295b91bcd4e3e7d8e7d16
Zile028 <zile028@gmail.com>
2022-05-13 13:55:31 +0200

    current weather

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import { useState, useEffect } from "react";
-import "./App.css";
+import "./App.scss";
+import CurrentWeather from "./Components/CurrentWeather/CurrentWeather";
 
 function App() {
-  const [currentWeather, setCurrentWeather] = useState([]);
+  const [weather, setWeather] = useState({});
+  const [city, setCity] = useState("London");
 
   const apiKey = "92fe8ed71d6c96ecf7fa577cadb248f5";
 
   useEffect(() => {
+    console.log("Render data");
     fetch(
-      `https://api.openweathermap.org/data/2.5/weather?lat=42.88&lon=20.86&appid=${apiKey}&units=metric`
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
+      // `https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&appid=92fe8ed71d6c96ecf7fa577cadb248f5&units=metric`
     )
       .then((res) => res.json())
-      .then((res) => setCurrentWeather(res));
-  }, []);
+      .then((res) => setWeather(res));
+  }, [city]);
 
   return (
-    <div>
-      <h1>
-        {currentWeather.name}, {currentWeather.sys.country}
-      </h1>
-      <p>Temperature: {currentWeather.main.temp}&deg;C</p>
+    <div className="container">
+      <div className="input-holder">
+        <input type="text" onInput={(e) => setCity(e.target.value)} />
+        <button>Find</button>
+      </div>
+      {weather.name && <CurrentWeather data={weather} />}
     </div>
   );
 }
---
+++ src/Components/CurrentWeather/CurrentWeather.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "./style.scss";
+import Moment from "react-moment";
+import "moment/locale/sr";
+
+function CurrentWeather({ data }) {
+  const weather = {
+    name: data.name,
+    country: data.sys.country,
+    temp: Math.round(data.main.temp) + "\u00b0C",
+    icon: `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`,
+    dateTime: data.dt,
+  };
+
+  return (
+    <div className="current-weather">
+      <Moment className="date-time" unix locale="sr" format="DD. MMM. Y">
+        {weather.dateTime}
+      </Moment>
+      <h2>
+        {weather.name}, {weather.country}
+      </h2>
+      <div>
+        <img src={weather.icon} alt="" />
+        <h3>{weather.temp}</h3>
+      </div>
+    </div>
+  );
+}
+
+export default CurrentWeather;
